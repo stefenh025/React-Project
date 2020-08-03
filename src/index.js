350714a7de49444ec222917d6394ed8bca2bc8cf
Stefen Huang <stefenxh@gmail.com>
2020-08-02 22:12:40 -0700

    Ahhhh

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Option extends React.Component{
           <option value="all">All</option> 
           <option value="cars">Cars</option>
           <option value="trucks">Trucks</option>
-          <option value="convertibles">Ahhhhh</option>
+          <option value="convertibles">Ahhhh</option>
         </select>
       </div>
     )
