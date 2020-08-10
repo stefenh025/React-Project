648f10f896dd8f1c0c02c6039162dac368efb5cf
Stefen Huang <stefenxh@gmail.com>
2020-08-10 00:24:11 -0700

    Pick a ** fix

--- src/vehicle.js
+++ src/vehicle.js
@@ -18,18 +18,18 @@ export default class Vehicle extends React.Component{
     else if (carType === 'cars'){
       return(
         <div>
-        <Car car={data.car} newOnly={this.props.newOnly} handleBuyClick={this.props.handleBuyClick}/>
+        <Car car={data.car} newOnly={this.props.newOnly} handleBuyClick={this.props.handleBuyClick} name="car"/>
         </div>
       )
     }    
     else if(carType === 'trucks'){
       return(
-        <Car car={data.truck} newOnly={this.props.newOnly} handleBuyClick={this.props.handleBuyClick}/>
+        <Car car={data.truck} newOnly={this.props.newOnly} handleBuyClick={this.props.handleBuyClick} name="truck"/>
       )
     }
     else if(carType === 'convertibles'){
       return(
-        <Car car={data.convertible} newOnly={this.props.newOnly} handleBuyClick={this.props.handleBuyClick}/>
+        <Car car={data.convertible} newOnly={this.props.newOnly} handleBuyClick={this.props.handleBuyClick} name="convertible"/>
       )
     }
     else{
